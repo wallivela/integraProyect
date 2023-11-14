388c95af428fb33d9133320b605f278900b7b98d
wallivela <waltervelaplay@gmail.com>
2023-11-13 22:49:34 -0500

    cuarto spring

--- back-end/src/controllers/user.controllers.js
+++ back-end/src/controllers/user.controllers.js
@@ -2,7 +2,7 @@ import Users from "../models/Users.js";
 
 export async function createUser(req, res) {
     let createUser = new Users(req.body);
-
+//--------------------------------------------------
     try {
         await createUser.save();
         res.status(200).json(createUser);
@@ -15,6 +15,21 @@ export async function getUser(req, res) {
     try {
         let usuarios = await Users.find(); // Espera a que la promesa se resuelva
         res.status(200).json(usuarios);
+    } catch (error) {
+        res.status(500).json("error");
+    }
+}
+//----------------------------------------------
+export async function login(req, res) {
+    try {
+        let correo = req.body.correo;
+        let pasword = req.body.pasword;
+        let usuario = await Users.findOne({ correo: correo }); // Cambié "usuarios" a "usuario"
+        if (pasword === usuario.pasword) {
+            res.status(200).json({ status: "estás logueado" });
+            return;
+        }
+        res.status(500).json({ status: "upss parece no estar logueado" });
     } catch (error) {
         res.status(500).json("error");
     }
--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
-import {createUser, getUser} from "../controllers/user.controllers.js";
+import {createUser, getUser, login} from "../controllers/user.controllers.js";
 
 const router = Router();
 
 router.post('/users/create', createUser)
 router.get('/users/get', getUser)
-
+router.post('/users/login',login)
 export default router
